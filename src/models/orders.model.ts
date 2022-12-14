dddfac4d24e7482119183d1dd04b13b99fe7c286
ItzMari0 <msajr@outlook.com>
2022-12-14 20:44:13 -0300

    correção de lint em OrdersModel

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -4,7 +4,8 @@ import connection from './connection';
 
 const getOrders = async (): Promise<TOrders[]> => {
   const [orders] = await connection.execute<RowDataPacket[] & TOrders[]>(
-    `SELECT a.id, a.user_id as userId, JSON_ARRAYAGG(b.id) as productsIds FROM Trybesmith.orders AS a
+    `SELECT a.id, a.user_id as userId, JSON_ARRAYAGG(b.id) as productsIds 
+    FROM Trybesmith.orders AS a
     INNER JOIN Trybesmith.products AS b WHERE b.order_id = a.id GROUP BY a.id;`,
   );
   return orders;
