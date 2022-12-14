5860232fb2b6e39de8c76a7542198ff1e0ef4a39
ItzMari0 <msajr@outlook.com>
2022-12-14 16:54:16 -0300

    rota da products criada e adicionada em app.ts. Requisito 1 resolvido

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import productsRouter from './routers/products.route';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productsRouter);
 
 export default app;
