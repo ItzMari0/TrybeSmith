f2d7d7a27f2b0ee418f70f20461117c260394adb
ItzMari0 <msajr@outlook.com>
2022-12-15 15:19:49 -0300

    Requisito 7 resolvido

--- src/routers/users.route.ts
+++ src/routers/users.route.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import usersController from '../controllers/users.controller';
+import validate from '../middlewares/userValidation.middleware';
 
 const router = Router();
 
-router.post('/', usersController.createUser);
+router.post(
+  '/',
+  validate.usernameValidation,
+  validate.vocationValidation,
+  validate.levelValidation,
+  validate.passwordValidation,
+  usersController.createUser,
+);
 
 export default router;
